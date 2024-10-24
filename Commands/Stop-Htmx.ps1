f5bb85e19fec9e3e72bab7b4aef482f3949c3c54
StartAutomating <StartAutomating@github.com>
2024-10-24 02:01:17 +0000

    feat: Stop-HTMX ( Fixes #8 )

---
+++ Commands/Stop-Htmx.ps1
@@ -0,0 +1,82 @@
+[ValidatePattern('Stop\p{P}+Htmx')]
+param()
+function Stop-Htmx {
+    <#
+    
+    .SYNOPSIS    
+        Stops an htmx server.    
+    .DESCRIPTION    
+        Stops an htmx server running in a background job.    
+    
+    #>
+            
+
+    param(
+    [Parameter(ParameterSetName='JobParameterSet', Mandatory=$true, Position=0, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+    [ValidateNotNullOrEmpty()]
+    [System.Management.Automation.Job[]]
+    ${Job},
+
+    [switch]
+    ${PassThru},
+
+    [Parameter(ParameterSetName='NameParameterSet', Mandatory=$true, Position=0, ValueFromPipelineByPropertyName=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string[]]
+    ${Name},
+
+    [Parameter(ParameterSetName='InstanceIdParameterSet', Mandatory=$true, Position=0, ValueFromPipelineByPropertyName=$true)]
+    [ValidateNotNullOrEmpty()]
+    [guid[]]
+    ${InstanceId},
+
+    [Parameter(ParameterSetName='SessionIdParameterSet', Mandatory=$true, Position=0, ValueFromPipelineByPropertyName=$true)]
+    [ValidateNotNullOrEmpty()]
+    [int[]]
+    ${Id},
+
+    [Parameter(ParameterSetName='StateParameterSet', Mandatory=$true, Position=0, ValueFromPipelineByPropertyName=$true)]
+    [System.Management.Automation.JobState]
+    ${State},
+
+    [Parameter(ParameterSetName='FilterParameterSet', Mandatory=$true, Position=0, ValueFromPipelineByPropertyName=$true)]
+    [ValidateNotNullOrEmpty()]
+    [hashtable]
+    ${Filter}
+    )
+    dynamicParam {
+    $baseCommand = 
+        if (-not $script:StopJob) {
+            $script:StopJob = 
+                $executionContext.SessionState.InvokeCommand.GetCommand('Stop-Job','Cmdlet')
+            $script:StopJob
+        } else {
+            $script:StopJob
+        }
+    $IncludeParameter = @()
+    $ExcludeParameter = @()
+
+    }
+        process {
+        if ($job) {
+            if ($job.HttpListener) {
+                $job.HttpListener.Stop()
+            }
+            $job | Stop-Job
+        } else {
+            foreach ($existingJob in Get-Job @PSBoundParameters) {
+                if ($existingJob.HttpListener) {
+                    try {
+                        $existingJob.HttpListener.Stop()
+                    } catch {
+                        Write-Verbose "Failed to stop the listener for job $($existingJob.Id): $_"
+                    }
+                    
+                }
+                $existingJob | Stop-Job
+            }
+        }
+    
+    }
+}
+
