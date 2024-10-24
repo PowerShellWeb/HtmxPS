ca8ef243308925664bf3a2c06f81acb0f4cd3657
James Brundage <+@noreply.github.com>
2024-10-23 19:00:17 -0700

    feat: Stop-HTMX ( Fixes #8 )

---
+++ Commands/Stop-Htmx.ps.ps1
@@ -0,0 +1,33 @@
+[ValidatePattern('Stop\p{P}+Htmx')]
+param()
+function Stop-Htmx {
+    <#
+    .SYNOPSIS
+        Stops an htmx server.
+    .DESCRIPTION
+        Stops an htmx server running in a background job.
+    #>
+    [inherit('Stop-Job', Abstract)]
+    param()
+
+    process {
+        if ($job) {
+            if ($job.HttpListener) {
+                $job.HttpListener.Stop()
+            }
+            $job | Stop-Job
+        } else {
+            foreach ($existingJob in Get-Job @PSBoundParameters) {
+                if ($existingJob.HttpListener) {
+                    try {
+                        $existingJob.HttpListener.Stop()
+                    } catch {
+                        Write-Verbose "Failed to stop the listener for job $($existingJob.Id): $_"
+                    }
+                    
+                }
+                $existingJob | Stop-Job
+            }
+        }
+    }
+}
